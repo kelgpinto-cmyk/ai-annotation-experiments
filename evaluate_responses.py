81f4125012a957f540c15381ab7c40efa8712a4d
kelgpinto-cmyk <kelgpinto@gmail.com>
2026-03-16 18:52:08 +0000

    Create evaluate_responses.py

---
+++ evaluate_responses.py
@@ -0,0 +1,9 @@
+responses = [
+    "The sky appears blue because shorter wavelengths of light scatter more in the atmosphere.",
+    "The sky is blue because the ocean reflects its color."
+]
+
+for r in responses:
+    print("Response:", r)
+    print("Length:", len(r))
+    print("---")
